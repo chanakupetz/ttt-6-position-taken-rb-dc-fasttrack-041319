22d73b666c18a59f0231e2a25d7cb267c8e1c36c
Chana Kupetz <chanakupetz@gmail.com>
2019-04-14 15:32:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,14 @@
 # code your #position_taken? method here!
 
+
 def position_taken?(board,index)
-  if board[index] == "" || " " || nil
+  if board[index] == " "
+    return false
+    elsif board[position] == ""
+    return false
+    elsif  board[position] == nil
+    return false
+    else   board[position] == "X" || "O"
     return true
-  elsif board[index] == "0" || "X"
-  
-  return false
-  
+  end
 end
