1cf3c95f10faf58324407996f6b2a76c37e73e73
Chana Kupetz <chanakupetz@gmail.com>
2019-04-14 15:43:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,3 +8,15 @@ def position_taken?(board,index)
     return true
   end
 end
+
+def position_taken?(board,index)
+  if board[index] == " "
+    return false
+    elsif board[index] == ""
+    return false
+    elsif  board[index] == nil
+    return false
+    else   board[index] == "X" || "O"
+    return true
+  end
+end
